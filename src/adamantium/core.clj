ed6d42aada6124ed09222557210f280ee0311f04
Adam Clements <adam.clements@gmail.com>
2012-05-27 01:33:10 +0100

    Remove half baked ideas now main thing is working

--- src/adamantium/core.clj
+++ src/adamantium/core.clj
@@ -1,17 +1,6 @@
 (ns adamantium.core
   (:use [clojure.stacktrace]))
 
-;(defonce resilient-fn-store (atom {}))
-
-#_(defn add-alternative-fn [name func]
-  (swap! resilient-fn-store assoc name (conj (name @resilient-fn-store) func)))
-
-#_(defn pop-alternative-fn [name default]
-  (let [fn-list (name @resilient-fn-store)
-        new-list (rest fn-list)]
-    (swap! resilient-fn-store assoc name new-list)
-    (peek (name @resilient-fn-store))))
-
 (defmacro defresilient
   "Use in place of defn; replaces the function with a resilient binding which
    catches exceptions, prints them to the console and instead of throwing, 
@@ -48,22 +37,3 @@
           (.printStackTrace exp)
           (swap! rollback-data pop)
           (apply example-resilient args))))))
-
-#_(defmacro defresilient
-  "Use in place of defn; replaces the function with a resilient binding which
-   catches exceptions, prints them to the console and instead of throwing, 
-   rebinds the function to one which returns a safe default"
-  [name default & definition]
-  `(do
-     (def ~name)
-     (let [f# (fn ~@definition)]
-       (defn ~name [& args#]
-         (try
-           (apply f# args#)
-           (catch Exception e#
-             (println (.getStackTrace e#)) ; This uses println instead of .printStackTrace so we can test it
-                                           ; easily with midje (doesn't support mocking java calls yet)
-             (defn ~name [& args#] (pop-alternative-fn '~name ~default))
-             (~name args#))))
-       (add-alternative-fn '~name ~name)
-       )))
