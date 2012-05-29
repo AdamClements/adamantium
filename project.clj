2aca697990c09350dcc89270ea852bda24f4346c
Adam Clements <adam@touchtype-online.com>
2012-05-29 03:20:22 +0100

    Brought midje into normal dependencies rather than dev-dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject adamantium "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A resilience library for better interactive programming"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [com.stuartsierra/lazytest "1.2.3"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [midje "1.3.1"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "1.2.3"]])
