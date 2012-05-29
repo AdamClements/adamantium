22e2a7b2524cde7195eb41d12f51e1fe0d3ea4db
Adam Clements <adam@touchtype-online.com>
2012-05-29 03:20:38 +0100

    Do away with unnecessary do

--- src/adamantium/core.clj
+++ src/adamantium/core.clj
@@ -6,21 +6,20 @@
    catches exceptions, prints them to the console and instead of throwing, 
    rolls back to a previous incarnation"
   [name default & definition]
-  `(do
-     (let [f# (fn ~@definition)]
-       (if-let [existing-rollbacks# (:rollbacks (meta (resolve '~name)))]
-         (swap! existing-rollbacks# conj f#)        ; This is already a resilient function, just add to the list of fns
-         (do                                                      ; Otherwise we have to create the wrapper
-           (defn ~name {:rollbacks (atom [(fn [& whatever#] ~default) f#])}
-             [& args#]
-             (let [rollback-data# (:rollbacks (meta (resolve '~name)))
-                   latest-fn#     (last @rollback-data#)]
-               (try (apply latest-fn# args#)
-                 (catch Exception exp#
-                   (do 
-                     (print-stack-trace exp#)
-                     (swap! rollback-data# pop)
-                     (apply ~name args#)))))))))))
+  `(let [f# (fn ~@definition)]
+     (if-let [existing-rollbacks# (:rollbacks (meta (resolve '~name)))]
+       (swap! existing-rollbacks# conj f#)        ; This is already a resilient function, just add to the list of fns
+       (do                                                      ; Otherwise we have to create the wrapper
+         (defn ~name {:rollbacks (atom [(fn [& whatever#] ~default) f#])}
+           [& args#]
+           (let [rollback-data# (:rollbacks (meta (resolve '~name)))
+                 latest-fn#     (last @rollback-data#)]
+             (try (apply latest-fn# args#)
+               (catch Exception exp#
+                 (do 
+                   (print-stack-trace exp#)
+                   (swap! rollback-data# pop)
+                   (apply ~name args#))))))))))
 
 #_(defn example [a b] (/ a b))
 
