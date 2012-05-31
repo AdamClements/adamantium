71259b6ab9025b9e1aaab4d5615eb95b4f7cb36d
Adam Clements <adam@touchtype-online.com>
2012-05-31 02:43:46 +0100

    Add in a variation which either defns or sets up a roll-back wrapper with no default

--- src/adamantium/core.clj
+++ src/adamantium/core.clj
@@ -2,7 +2,7 @@
   (:use [clojure.stacktrace]))
 
 (defmacro defresilient
-  "Use in place of defn; replaces the function with a resilient binding which
+  "A wrapper around a function with a resilient binding which
    catches exceptions, prints them to the console and instead of throwing, 
    rolls back to a previous incarnation"
   [name default & definition]
@@ -19,3 +19,27 @@
                  (print-stack-trace exp#)
                  (swap! rollback-data# pop)
                  (apply ~name args#)))))))))
+
+(defmacro defnr
+  "Either defines or redefines a function. When redefining, if the new
+   function fails, it will roll back to a previous incarnation"
+  [name & definition]
+  `(cond (:rollbacks (meta (resolve '~name)))
+         (swap! (:rollbacks (meta (resolve '~name))) conj (fn ~@definition))
+        
+         (resolve '~name)
+         (if-let [orig-fn# name]
+           (defn ~name 
+             {:rollbacks (atom [orig-fn# (fn ~@definition)])}
+             [& args#]
+             (let [rollbacks# (:rollbacks (meta (resolve '~name)))
+                   latest-fn# (last @rollbacks#)]
+               (try (apply latest-fn# args#)
+                 (catch Exception exp#
+                   (do
+                     (print-stack-trace exp#)
+                     (swap! rollbacks# pop)
+                     (apply ~name args#)))))))
+  
+         :else
+         (def ~name (fn ~@definition))))
